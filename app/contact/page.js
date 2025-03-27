447fccc8b3ce460c774b110ce08a73ec4dbabfa1
betty <bhetea001@gmail.com>
2025-03-27 15:46:08 +0100

    Create About and Contact Page.

---
+++ app/contact/page.js
@@ -0,0 +1,5 @@
+export default function Contact() {
+    return(
+        <h1>Welcome to the contact page</h1>
+    );
+}
---
+++ app/page.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <nav>
+        <ul>
+          <li><Link href="/about">About</Link></li>
+          <li><Link href="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
