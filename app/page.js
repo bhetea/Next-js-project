a6c2aa61b3cd5131a8b103bff851517c8d70118a
betty <bhetea001@gmail.com>
2025-03-29 19:25:22 +0100

    Create navbar component

--- app/page.js
+++ app/page.js
@@ -5,13 +5,6 @@ export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
-      <nav>
-        <ul>
-          <li><Link href="/about">About</Link></li>
-          <li><Link href="/contact">Contact</Link></li>
-        </ul>
-      </nav>
-
       <Users />
     </div>
   );
